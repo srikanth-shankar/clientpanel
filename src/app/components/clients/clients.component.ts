c2b9f9feaeaff1d09a571bf2ca7a5a4764550920
Srikanth Shankar <srikanth.shankar2@gmail.com>
2017-08-21 13:50:50 +1000

    added binding for total owed

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -8,15 +8,28 @@ import {IClient} from '../../models/Client';
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
-  clients:IClient[] = [];
+  clients:any[] = [];
+  totalOwed : number;
   constructor(public cs : ClientService) { }
 
   ngOnInit() {
     this.cs.getClients().subscribe(clients=>{
       this.clients = clients;
       //debugger;
-      console.log(this.clients);
+      //console.log(this.clients);
+      this.getTotalOwed();
     });
+  }
+
+  getTotalOwed()
+  {
+    let total = 0;
+    for(let i=0; i<this.clients.length; i++)
+      {
+        total += parseFloat(this.clients[i].balance);
+      }
+      this.totalOwed = total;
+      console.log(this.totalOwed);
   }
 
 }
